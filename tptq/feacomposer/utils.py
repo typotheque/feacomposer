dc5277a13c22bd15233282e50615f2c96b60de7b
Liang Hai <lianghai@gmail.com>
2024-01-12 13:08:48 +0100

    Collect and cache name mapping

--- tptq/feacomposer/utils.py
+++ tptq/feacomposer/utils.py
@@ -7,7 +7,7 @@ from fontTools.feaLib.parser import Parser
 
 class GlyphRenamingParser(Parser):
     """
-    All glyph names are renamed with the `renamer` function during parsing, before each (new, post-renaming) name is validated against the optional `newGlyphNames` iterable.
+    All glyph names are renamed with the `renamer` function during parsing, before each (new, post-renaming) name is validated against the optional `newNames` iterable.
     """
 
     def __init__(
@@ -15,17 +15,22 @@ class GlyphRenamingParser(Parser):
         featurefile: TextIO | PathLike[str] | str,
         renamer: Callable[[str], str],
         *,
-        newGlyphNames: Iterable[str] = (),
+        newNames: Iterable[str] = (),
         followIncludes=True,
-        includeDir: PathLike[str] | str | None = None
+        includeDir: PathLike[str] | str | None = None,
     ):
         self.renamer = renamer
+        self.oldNameToNew = dict[str, str]()
         super().__init__(
             featurefile,
-            glyphNames=newGlyphNames,
+            glyphNames=newNames,
             followIncludes=followIncludes,
             includeDir=includeDir,
         )
 
-    def expect_glyph_(self):
-        return self.renamer(super().expect_glyph_())
+    def expect_glyph_(self) -> str:
+        old = super().expect_glyph_()
+        new = self.oldNameToNew.get(old)
+        if not new:
+            self.oldNameToNew[old] = new = self.renamer(old)
+        return new
